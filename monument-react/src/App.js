50da6ce696541aa6bf02cfa85b781b387c06e112
Howakyl96 <howakyl@gmail.com>
2020-11-09 16:43:19 -0800

    initial commit - navbar and landing header showing

---
+++ monument-react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Blog from './Pages/Blog';
+import Landing from './Pages/Landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ monument-react/src/Pages/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/LandingHeader';
+import './Landing.css';
+import articleImageOne from '../images/article_1.jpg';
+
+const Landing  = (props) => {
+    return (
+        <div>
+            <Header />
+            <img src={articleImageOne} alt="cactus"/>
+        </div>
+    )
+};
+
+export default Landing;
