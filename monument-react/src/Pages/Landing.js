952a4d6c8c5d8e92402213ad01ce9220196c9975
Howakyl96 <howakyl@gmail.com>
2020-11-11 17:02:51 -0800

    final touches, removed commented out code

--- monument-react/src/Pages/Landing.js
+++ monument-react/src/Pages/Landing.js
@@ -5,13 +5,11 @@ import About from '../components/About';
 import Gallery from '../components/Gallery';
 import './Landing.css';
 import Contact from '../components/Contact';
-// import articleImageOne from '../images/article_1.jpg';
 
 const Landing  = (props) => {
     return (
         <div>
             <Header />
-            {/* <img src={articleImageOne} alt="cactus"/> */}
             <Archive />
             <About />
             <Gallery />
