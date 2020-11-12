b72e2e42ebbd090368ef1fcd97d6fe1264e8b79d
Howakyl96 <howakyl@gmail.com>
2020-11-11 16:30:43 -0800

    added gallery on ladning view

--- monument-react/src/Pages/Landing.js
+++ monument-react/src/Pages/Landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/LandingHeader';
 import Archive from '../components/Archive';
 import About from '../components/About';
+import Gallery from '../components/Gallery';
 import './Landing.css';
 // import articleImageOne from '../images/article_1.jpg';
 
@@ -12,6 +13,7 @@ const Landing  = (props) => {
             {/* <img src={articleImageOne} alt="cactus"/> */}
             <Archive />
             <About />
+            <Gallery />
         </div>
     )
 };
